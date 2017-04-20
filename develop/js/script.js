be104214e5125ce247f50242792c4ad4838d4845
Ростислав <extra0@mail.ru>
2017-04-20 22:37:23 +0300

    index 100%

--- develop/js/script.js
+++ develop/js/script.js
@@ -8,5 +8,26 @@ $(function(){
 		controls: false
 	});
 
+	// смена класса по id
+	$('[data-toggle]').on('click', function(){
+		$('#'+ $(this).attr('data-toggle') +'').toggleClass('active');
+		$(this).toggleClass('active');
+	});
+
+	// // убираем меню по клику вне области
+	// $(document).mouseup(function(e) {
+	// 	console.log($(e.target).hasClass('active'));
+	// 	if ($(e.target).hasClass('active')) {
+	// 		$('#catalog-menu, #main-menu').removeClass('active');
+	// 		$('.header [data-toggle]').removeClass('active');
+	// 		return false;
+	// 	}
+	// 	if ($('#catalog-menu').has(e.target).length === 0 || $('#main-menu').has(e.target).length === 0) {
+	// 		$('#catalog-menu, #main-menu').removeClass('active');
+	// 		$('.header [data-toggle]').removeClass('active');
+	// 	}
+
+	// });
+
 
 });
